f92c24a37dde6c67142d6963e1bb5b7b185f178c
Mazz Mosley <marianne.mosley@gmail.com>
2014-03-28 15:02:34 +0000

    Include how many observations dataset has

--- lib/presenters/dataset_presenter.rb
+++ lib/presenters/dataset_presenter.rb
@@ -9,6 +9,7 @@ class DatasetPresenter
       "title" => @dataset.title,
       "slug" => @dataset.slug,
       "description" => @dataset.description,
+      "total_observations" => @dataset.observations.count,
       "release" => ReleasePresenter.new( @dataset.release ).present
     }
   end
