3468dc839efcff6450373319f475fa884b2f7fed
Leigh Dodds <leigh@ldodds.com>
2014-04-01 11:15:53 +0100

    Add next_release and filter list of releases

--- lib/presenters/series_presenter.rb
+++ lib/presenters/series_presenter.rb
@@ -4,11 +4,14 @@ class SeriesPresenter < ModelPresenter
     presented = default
     presented["contact"] = ContactPresenter.new( @model.contact ).present if @model.contact
     presented["releases"] = []
-    @model.releases.order_by(:slug.asc).each do |release|
+    @model.releases.where(:state=>"released").order_by(:slug.asc).each do |release|
       presented["releases"] << ModelPresenter.new( release ).present
     end
     latest_release = @model.releases.where(:state=>"released").order_by(:slug.desc).first
     presented["latest_release"] = ModelPresenter.new( latest_release ).present
+    next_release = @model.releases.where(:state=>"scheduled").order_by(:slug.desc).first
+    presented["next_release"] = ModelPresenter.new( next_release ).present
+
     presented
   end
 end
